578b0d5de0b71b7fe94ab146cc667d05d54eb8a1
Gonçalo Simões <garss@iscte-iul.pt>
2018-05-11 22:53:10 +0100

    20180511 - Menu added, login bypassed, now with Frags

--- app/src/main/java/com/example/ruijs/aeiscte/MenuActivity.java
+++ app/src/main/java/com/example/ruijs/aeiscte/MenuActivity.java
@@ -1,13 +1,66 @@
 package com.example.ruijs.aeiscte;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements
+        InfoFragment.OnFragmentInteractionListener
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+
+    // fragments saved to avoid resets
+    private Fragment info_frag = new InfoFragment();
+
+    // testing TAG
+    private String TAG = "nossa_tag";
+
+    NavigationView.OnNavigationItemSelectedListener navListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Log.d(TAG, "li ->>> " + item.getItemId());
+            switch (item.getItemId()) {
+                case R.id.db:
+                    Log.d(TAG, "CLICASTE NO FEED");
+                    return true;
+                case R.id.event:
+                    Log.d(TAG, "CLICASTE NO EVENTS");
+                    return true;
+                case R.id.settings:
+                    Log.d(TAG, "CLICASTE NO SETTINGS");
+                    showInfo();
+                    return true;
+                case R.id.logout:
+                    Log.d(TAG, "CLICASTE NO LOGOUT");
+                    return true;
+            }
+            return false;
+
+        }
+    };
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-    }
-}
+        NavigationView navView = (NavigationView) findViewById(R.id.navigationView);
+        navView.setNavigationItemSelectedListener(navListener);
+        }
+
+protected void showInfo(){
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, info_frag).commit();
+        }
+
+@Override
+public void onFragmentInteraction(Uri uri) {
+        Log.d(TAG,"U triggered fragment interaction");
+        }
+        }
