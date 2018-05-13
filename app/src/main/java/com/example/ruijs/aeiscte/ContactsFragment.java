f8fc8dba0711272499326dae1e739e26f4e25360
drmse@iscte-iul.pt <EuISCTE2014>
2018-05-13 12:59:05 +0100

    Já com números de telefone

--- app/src/main/java/com/example/ruijs/aeiscte/ContactsFragment.java
+++ app/src/main/java/com/example/ruijs/aeiscte/ContactsFragment.java
@@ -1,14 +1,31 @@
 package com.example.ruijs.aeiscte;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ContactsFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class ContactsFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private final LatLng ISCTE = new LatLng(38.748753, -9.153692);
 
     @Nullable
     @Override
@@ -19,8 +36,36 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        // init map view
+        MapView mapView = (MapView) view.findViewById(R.id.mapView);
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        map = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(ISCTE).title("ISCTE-IUL"));
+
+        CameraPosition toIscte = CameraPosition.builder().target(ISCTE).zoom(16).bearing(0).tilt(45).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(toIscte));
+    }
+
+    // do not erase can be useful in future
+    public  void DrawLine() {
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.add(new LatLng(ISCTE.latitude-50, ISCTE.longitude-80))
+                .add(new LatLng(ISCTE.latitude-10, ISCTE.longitude-50))
+                .add(ISCTE);
+        map.addPolyline(polylineOptions);
+    }
 
     /*
 
