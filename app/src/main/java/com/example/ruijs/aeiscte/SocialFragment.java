b34b186eb98c2230500cd5f72f044948dce0c800
rjsps-iscteiul <rjsps@iscte-iul.pt>
2018-05-15 23:53:54 +0100

    20180516 - Fragmento das redes sociais. Ja encaminha para externo

--- app/src/main/java/com/example/ruijs/aeiscte/SocialFragment.java
+++ app/src/main/java/com/example/ruijs/aeiscte/SocialFragment.java
@@ -1,33 +1,75 @@
 package com.example.ruijs.aeiscte;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class SocialFragment extends Fragment {
+import java.util.List;
+
+public class SocialFragment extends Fragment{
+
+    public static String FACEBOOK_URL = "https://www.facebook.com/aeiscte";
+    public static String FACEBOOK_PAGE_ID = "aeiscte";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_social, null);
-    }
+            }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.ivInstagram).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://www.instagram.com/aeiscte/");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        view.findViewById(R.id.ivFacebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://www.facebook.com/aeiscte/");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        view.findViewById(R.id.ivTwitter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://twitter.com/AEISCTE");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        view.findViewById(R.id.ivYouTube).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://www.youtube.com/user/aeiscte");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+
     }
 
-
-    /*
-
-        **********************************
-        CODE WILL BE HERE AND ON THE ONCREATED METHODS IF NECESSARY
-        **********************************
-
-     */
 
 }
