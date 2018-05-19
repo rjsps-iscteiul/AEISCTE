094ad4dc7f4401ee6634f67a2c7a6cedee6cb219
Jorge Rafael Santos <jrcss11@iscte-iul.pt>
2018-05-19 02:22:07 +0100

    Fragment dos detalhes do feed a ser criado. Falta fazer quando se clica no feed abre o fragment detalhado.

--- app/src/main/java/com/example/ruijs/aeiscte/FeedFragment.java
+++ app/src/main/java/com/example/ruijs/aeiscte/FeedFragment.java
@@ -18,6 +18,9 @@ public class FeedFragment extends Fragment {
 
         LinearLayout linearLayout = (LinearLayout) getActivity().findViewById(R.id.feed_linearLayout);
 
+
+        //Depois é preciso saber quantas noticias estão na base de dados e criar um for para as primeiras 20 (por exemplo)
+        //Podiamos ver se, a pessoa vai muito para baixo é que carrega mais noticias
         Fragment card = new Card();
         getFragmentManager().beginTransaction().add(R.id.feed_linearLayout, new Card()).commit();
         getFragmentManager().beginTransaction().add(R.id.feed_linearLayout, new Card()).commit();
