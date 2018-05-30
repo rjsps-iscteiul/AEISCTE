f6183ad8a78f4fa85960cb01ca30867af8f1885a
Gonçalo Simões <garss@iscte-iul.pt>
2018-05-30 22:32:57 +0100

    Merge branch 'master' of C:\Users\gr_si\AndroidStudioProjects\AEISCTE with conflicts.

--- app/src/main/java/com/example/ruijs/aeiscte/ProfileFragment.java
+++ app/src/main/java/com/example/ruijs/aeiscte/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.KeyListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,30 +14,35 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.internal.api.FirebaseNoSignedInUserException;
 
 public class ProfileFragment extends Fragment {
 
-    //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference databaseReference = database.getReference();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-    DatabaseReference myRef = database.getReference("message");
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference("Users");
+
     boolean is_editing = false;
     EditText et_user_name, et_curso , et_email, et_birth , et_phone;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        et_user_name = getActivity().findViewById(R.id.user_name);
-        et_curso = getActivity().findViewById(R.id.curso_text);
-        et_email = getActivity().findViewById(R.id.email_text);
-        et_birth = getActivity().findViewById(R.id.birth_text);
-        et_phone = getActivity().findViewById(R.id.phone_text);
 
-        return inflater.inflate(R.layout.fragment_profile, null);
+        View myView = inflater.inflate(R.layout.fragment_profile, null);
+        et_user_name = myView.findViewById(R.id.user_name);
+        et_curso = myView.findViewById(R.id.curso_text);
+        et_email = myView.findViewById(R.id.email_text);
+        et_birth = myView.findViewById(R.id.birth_text);
+        et_phone = myView.findViewById(R.id.phone_text);
+        return myView;
+
     }
 
     @Override
@@ -49,17 +55,48 @@ public class ProfileFragment extends Fragment {
                 //EDITAR INFO
                 if(is_editing == false){
                     is_editing = true;
+                    //et_user_name.setFocusable(true);
+                    //et_user_name.setFocusableInTouchMode(true);
+                    //et_user_name.setClickable(true);
                     btn.setText("Guardar Alterações");
                 //GUARDAR INFO
                 }else {
                     is_editing = false;
+                    final User utilizador = new User(et_user_name.getText().toString(), et_email.getText().toString(), et_curso.getText().toString(), et_birth.getText().toString(), Integer.parseInt(et_phone.getText().toString()));
+                    inserirUser(utilizador);
+                    //et_user_name.setEnabled(false);
+                    //et_curso.enable setEnabled(false);
+                    //et_email.setEnabled(false);
+                    //et_birth.setEnabled(false);
+                    //et_phone.setEnabled(false);
+
                     btn.setText("Editar Perfil");
                 }
-
-
             }
         });
 
+    }
+
+    public void inserirUser(User user1){
+        //final User user1 = new User(et_user_name.getText().toString(), et_email.getText().toString(), et_curso.getText().toString(), et_email.getText().toString(), Integer.parseInt(et_phone.getText().toString()));
+        //databaseReference.child(user.getEmail()).addValueEventListener(new ValueEventListener() {
+            //@Override
+            //public void onDataChange(DataSnapshot dataSnapshot) {
+                //if(user.getEmail().equals(dataSnapshot.getKey())){ //CASO EXISTA ESTA CHAVE NA BASE DE DADOS
+                    databaseReference.child(createKey(user1.getEmail())).setValue(user1);
+                //}
+            //}
+
+            //@Override
+            //public void onCancelled(DatabaseError databaseError) {
+
+            //}
+        //});
+    }
+
+    public String createKey(String email){
+        String [] email1 = email.split("@");
+        return email1[0];
     }
 
     /*
---
+++ app/src/main/java/com/example/ruijs/aeiscte/User.java
@@ -0,0 +1,57 @@
+package com.example.ruijs.aeiscte;
+
+public class User {
+    private String user_name;
+    private String email;
+    private String curso;
+    private String birth;
+    private int phone;
+
+    public User(String user_name, String email, String curso, String birth, int phone) {
+        this.user_name = user_name;
+        this.email = email;
+        this.curso = curso;
+        this.birth = birth;
+        this.phone = phone;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
