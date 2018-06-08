12016e68463a83a7571f7138202df2e70468a8fc
Jorge Rafael Santos <jrcss11@iscte-iul.pt>
2018-06-08 21:33:50 +0100

    FINAL

--- app/src/main/java/com/example/ruijs/aeiscte/FeedFactory.java
+++ app/src/main/java/com/example/ruijs/aeiscte/FeedFactory.java
@@ -37,15 +37,15 @@ public class FeedFactory {
                 "\n" +
                 "Prepara-te, o countdown já começou e os bilhetes estarão à venda muito em breve.");
         News feed2 = new News("Arraial do ISCTE", "Festas", "25/05/2018", R.drawable.event, true, true, "2");
-        feed2.setEventDate("08/06/2018");
+        feed2.setEventDate("22/06/2018");
         feed2.setLocal("Pátio 1");
         feed2.setPrice(5);
-        feed2.setText("A Comissão de Trabalhadores convida todos os trabalhadores do ISCTE-IUL (docentes, investigadores e técnicos) para o seu 1º Arraial a realizar no dia 8 de junho, a partir das 18h, no átrio exterior do Edifício Sedas Nunes.\n" +
+        feed2.setText("A Comissão de Trabalhadores convida todos os trabalhadores do ISCTE-IUL (docentes, investigadores e técnicos) para o seu 1º Arraial a realizar no dia 22 de junho, a partir das 18h, no átrio exterior do Edifício Sedas Nunes.\n" +
                 "\n" +
                 "Juntemo-nos neste fim de tarde, acompanhados pelas sardinhas assadas, bifanas, música popular portuguesa e, claro, o obrigatório bailarico!\n" +
                 "\n" +
                 "O valor da inscrição é de 5 sardinhas.\n" +
-                "Inscreve-te e entrega o valor da inscrição aos colegas Henrique Borges (SIIC), Rosário Candeias (SAS) ou Tiago Santos (GCM), até ao dia 6 de junho.\n" +
+                "Inscreve-te e entrega o valor da inscrição aos colegas Henrique Borges (SIIC), Rosário Candeias (SAS) ou Tiago Santos (GCM), até ao dia 20 de junho.\n" +
                 "\n" +
                 "Para qualquer esclarecimento, por favor contacta-nos através do e-mail ct@iscte-iul.pt");
         News feed3 = new News("Habemos NET!", "Núcleos", "24/05/2018", R.drawable.news, false, false, "3");
@@ -55,11 +55,14 @@ public class FeedFactory {
         "A tomada de posse será no dia 4 de junho pelas 13h30 no auditório B2.04. \n" +
         "\n"+
         "Contamos contigo!");
-        News feed4 = new News("Sèrgio é pai", "ETI", "28/05/2018", R.drawable.news, false, false, "4");
-        feed4.setText("O professor de Eletromagnetismo e de Propagação e Radiação de Ondas Eletromagnéticas, Sérgio Matos, foi pai de uma menina. \n" +
-        "O nascimento da filha obrigou ao adiamente do teste de PROE para a semana seguinte, dia 4 de junho. \n" +
-        "Desejamos as maiores felicidades.\n");
-        News feed5 = new News("Santos António", "Festas", "06/06/2018", R.drawable.event, true, true, "5");
+        News feed4 = new News("Nova reitora", "ISCTE", "09/02/2018", R.drawable.news, false, false, "4");
+        feed4.setText("O Conselho Geral elegeu a Professora Maria de Lurdes Rodrigues para ser a Reitora do ISCTE-IUL no período 2018-2022. É a primeira vez que uma mulher vai liderar a instituição.\n" +
+                "\n" +
+                "Maria de Lurdes Rodrigues é Professora Associada com Agregação no ISCTE-IUL, instituição onde se licenciou e doutorou em Sociologia e onde leciona desde 1986. É investigadora do CIES-IUL, Centro de Investigação e Estudos de Sociologia.\n" +
+                "Entre 2005 e 2009 foi Ministra da Educação e entre 2010 e 2013 foi presidente do Conselho de Administração da FLAD-Fundação Luso-Americana para o Desenvolvimento.\n" +
+                "\n" +
+                "Ao Conselho de Curadores cabe a homologação da decisão do Conselho Geral, em reunião que decorrerá nos próximos dias.");
+        News feed5 = new News("Santo António", "Festas", "06/06/2018", R.drawable.event, true, true, "5");
         feed5.setEventDate("12/06/2018");
         feed5.setPrice(2);
         feed5.setLocal("Lisboa");
